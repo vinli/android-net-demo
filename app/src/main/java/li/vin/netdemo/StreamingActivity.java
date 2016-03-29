ba9e66fc5f3bbac370aeecb9d0682898f51a7dab
Tommy Brown <tommy@vin.li>
2016-03-29 15:43:47 -0500

    StreamingActivity now displays the stream of data

--- app/src/main/java/li/vin/netdemo/StreamingActivity.java
+++ app/src/main/java/li/vin/netdemo/StreamingActivity.java
@@ -5,16 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import li.vin.net.Device;
 import li.vin.net.StreamMessage;
+import li.vin.net.StreamMessage.DataType;
 import li.vin.net.Vinli;
 import li.vin.net.VinliApp;
 import rx.Subscriber;
@@ -29,12 +30,19 @@ public class StreamingActivity extends AppCompatActivity implements OnMapReadyCa
     private Device device;
     private GoogleMap googleMap;
 
+    @Bind(R.id.rpm) TextView rpmTextView;
+    @Bind(R.id.vehicle_speed) TextView vehicleSpeedTextView;
+    @Bind(R.id.mass_air_flow)TextView massAirFlowTextView;
+    @Bind(R.id.calculated_load_value) TextView calculatedLoadValueTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_streaming);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
 
         vinliApp = Vinli.loadApp(this);
 
@@ -61,33 +69,31 @@ public class StreamingActivity extends AppCompatActivity implements OnMapReadyCa
         if (vinliApp == null) return;
 
         cleanupSubscription();
-
         subscription = new CompositeSubscription();
-
         ConnectableObservable<StreamMessage> stream = device.stream().publish();
 
         subscription.add(stream
-                .flatMap(StreamMessage.onlyWithIntVal(StreamMessage.DataType.RPM))
+                .flatMap(StreamMessage.onlyWithIntVal(DataType.RPM))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<Integer>() {
                     @Override
                     public void onCompleted() {
-
                     }
 
                     @Override
                     public void onError(Throwable e) {
-
+                        rpmTextView.setText(String.format(getString(R.string.error_fmt), DataType.RPM));
                     }
 
                     @Override
                     public void onNext(Integer rpm) {
                         Log.e("TESTO", "stream RPM for " + device.name() + " : " + rpm);
+                        rpmTextView.setText(String.format(getString(R.string.success_fmt), DataType.RPM, rpm));
                     }
                 }));
 
         subscription.add(stream
-                .flatMap(StreamMessage.onlyWithIntVal(StreamMessage.DataType.VEHICLE_SPEED))
+                .flatMap(StreamMessage.onlyWithIntVal(DataType.VEHICLE_SPEED))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<Integer>() {
                     @Override
@@ -97,17 +103,18 @@ public class StreamingActivity extends AppCompatActivity implements OnMapReadyCa
 
                     @Override
                     public void onError(Throwable e) {
-
+                        vehicleSpeedTextView.setText(String.format(getString(R.string.error_fmt), DataType.VEHICLE_SPEED));
                     }
 
                     @Override
                     public void onNext(Integer vss) {
                         Log.e("TESTO", "stream VSS for " + device.name() + " : " + vss);
+                        vehicleSpeedTextView.setText(String.format(getString(R.string.success_fmt), DataType.VEHICLE_SPEED, vss));
                     }
                 }));
 
         subscription.add(stream
-                .flatMap(StreamMessage.onlyWithFloatVal(StreamMessage.DataType.MASS_AIRFLOW))
+                .flatMap(StreamMessage.onlyWithFloatVal(DataType.MASS_AIRFLOW))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<Float>() {
                     @Override
@@ -117,17 +124,18 @@ public class StreamingActivity extends AppCompatActivity implements OnMapReadyCa
 
                     @Override
                     public void onError(Throwable e) {
-
+                        massAirFlowTextView.setText(String.format(getString(R.string.error_fmt), DataType.MASS_AIRFLOW));
                     }
 
                     @Override
                     public void onNext(Float maf) {
                         Log.e("TESTO", "stream MAF for " + device.name() + " : " + maf);
+                        massAirFlowTextView.setText(String.format(getString(R.string.success_fmt), DataType.MASS_AIRFLOW, maf));
                     }
                 }));
 
         subscription.add(stream
-                .flatMap(StreamMessage.onlyWithFloatVal(StreamMessage.DataType.CALCULATED_ENGINE_LOAD))
+                .flatMap(StreamMessage.onlyWithFloatVal(DataType.CALCULATED_ENGINE_LOAD))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<Float>() {
                     @Override
@@ -137,12 +145,13 @@ public class StreamingActivity extends AppCompatActivity implements OnMapReadyCa
 
                     @Override
                     public void onError(Throwable e) {
-
+                        calculatedLoadValueTextView.setText(String.format(getString(R.string.error_fmt), DataType.CALCULATED_ENGINE_LOAD));
                     }
 
                     @Override
                     public void onNext(Float load) {
                         Log.e("TESTO", "stream load% for " + device.name() + " : " + load);
+                        calculatedLoadValueTextView.setText(String.format(getString(R.string.success_fmt), DataType.CALCULATED_ENGINE_LOAD, load));
                     }
                 }));
 
