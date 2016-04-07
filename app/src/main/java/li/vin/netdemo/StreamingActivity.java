09dd6bad7af2f129234a24961f8caa8d35f9b942
Tommy Brown <tommy@vin.li>
2016-04-07 13:58:08 -0500

    Added server side filtering for rpm and vehicle speed. Formatted StreamingActivity

--- app/src/main/java/li/vin/netdemo/StreamingActivity.java
+++ app/src/main/java/li/vin/netdemo/StreamingActivity.java
@@ -18,12 +18,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import li.vin.net.Coordinate;
 import li.vin.net.Device;
 import li.vin.net.StreamMessage;
 import li.vin.net.StreamMessage.DataType;
+import li.vin.net.StreamMessage.ParametricFilter;
 import li.vin.net.Vinli;
 import li.vin.net.VinliApp;
 import rx.Subscriber;
@@ -31,67 +34,77 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.observables.ConnectableObservable;
 import rx.subscriptions.CompositeSubscription;
 
-public class StreamingActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class StreamingActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private CompositeSubscription subscription;
-    private VinliApp vinliApp;
-    private Device device;
-    private GoogleMap googleMap;
-    private Marker deviceMarker;
+  private CompositeSubscription subscription;
+  private VinliApp vinliApp;
+  private Device device;
+  private GoogleMap googleMap;
+  private Marker deviceMarker;
 
-    @Bind(R.id.rpm) TextView rpmTextView;
-    @Bind(R.id.vehicle_speed) TextView vehicleSpeedTextView;
-    @Bind(R.id.mass_air_flow)TextView massAirFlowTextView;
-    @Bind(R.id.calculated_load_value) TextView calculatedLoadValueTextView;
-    @Bind(R.id.intake_manifold_pressure) TextView intakeManifoldPressureTextView;
-    @Bind(R.id.engine_coolant_temp) TextView engineCoolantTempTextView;
-    @Bind(R.id.throttle_position) TextView throttlePositionTextView;
-    @Bind(R.id.time_since_engine_start) TextView timeSinceEngineStartTextView;
-    @Bind(R.id.fuel_rail_pressure) TextView fuelRailPressureTextView;
-    @Bind(R.id.fuel_pressure) TextView fuelPressureTextView;
-    @Bind(R.id.intake_air_temperature) TextView intakeAirTemperatureTextView;
-    @Bind(R.id.timing_advance) TextView timingAdvanceTextView;
+  @Bind(R.id.rpm) TextView rpmTextView;
+  @Bind(R.id.vehicle_speed) TextView vehicleSpeedTextView;
+  @Bind(R.id.mass_air_flow) TextView massAirFlowTextView;
+  @Bind(R.id.calculated_load_value) TextView calculatedLoadValueTextView;
+  @Bind(R.id.intake_manifold_pressure) TextView intakeManifoldPressureTextView;
+  @Bind(R.id.engine_coolant_temp) TextView engineCoolantTempTextView;
+  @Bind(R.id.throttle_position) TextView throttlePositionTextView;
+  @Bind(R.id.time_since_engine_start) TextView timeSinceEngineStartTextView;
+  @Bind(R.id.fuel_rail_pressure) TextView fuelRailPressureTextView;
+  @Bind(R.id.fuel_pressure) TextView fuelPressureTextView;
+  @Bind(R.id.intake_air_temperature) TextView intakeAirTemperatureTextView;
+  @Bind(R.id.timing_advance) TextView timingAdvanceTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_streaming);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_streaming);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        ButterKnife.bind(this);
+    ButterKnife.bind(this);
 
-        vinliApp = Vinli.loadApp(this);
+    vinliApp = Vinli.loadApp(this);
 
-        Intent intent = getIntent();
-        this.device = intent.getParcelableExtra(getString(R.string.streaming_device_key));
+    Intent intent = getIntent();
+    this.device = intent.getParcelableExtra(getString(R.string.streaming_device_key));
 
-        setTitle(device.name());
+    setTitle(device.name());
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        .findFragmentById(R.id.map);
+    mapFragment.getMapAsync(this);
 
-        subscribeAll();
-    }
+    subscribeAll();
+  }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
 
-        cleanupSubscription();
-    }
+    cleanupSubscription();
+  }
 
-    /** Permissively subscribe to all data. Clean up beforehand if necessary. */
-    private void subscribeAll() {
-        if (vinliApp == null) return;
+  /**
+   * Permissively subscribe to all data. Clean up beforehand if necessary.
+   */
+  private void subscribeAll() {
+    if (vinliApp == null) return;
 
-        cleanupSubscription();
-        subscription = new CompositeSubscription();
-        ConnectableObservable<StreamMessage> stream = device.stream().publish();
+    cleanupSubscription();
+    subscription = new CompositeSubscription();
 
-        /** Add a subscription to the CompositeSubscription. Filter for only messages that contain an RPM update.
-         *  Making sure to observeOn the main thread since we are updating a TextView. */
+    /**
+     *   Add filters to the stream. Only messages containing BOTH an RPM and a VEHICLE_SPEED will be sent to the app.
+     */
+    ArrayList<ParametricFilter.Seed> filterList = new ArrayList<>();
+    filterList.add(ParametricFilter.create().parameter(DataType.RPM.toString()).build());
+    filterList.add(ParametricFilter.create().parameter(DataType.VEHICLE_SPEED.toString()).build());
+
+    ConnectableObservable<StreamMessage> stream = device.stream(filterList, null).publish();
+
+    /** Add a subscription to the CompositeSubscription. This subscription only gets called when RPM is in the latest stream message.
+     *  Making sure to observeOn the main thread since we are updating a TextView. */
         subscription.add(stream
                 .flatMap(StreamMessage.onlyWithIntVal(DataType.RPM))
                 .observeOn(AndroidSchedulers.mainThread())
@@ -364,61 +377,65 @@ public class StreamingActivity extends AppCompatActivity implements OnMapReadyCa
                     }
                 }));
 
-        stream.connect();
+    stream.connect();
 
+  }
+
+  /**
+   * Unsubscribe all. Need to call this to clean up rx resources.
+   */
+  private void cleanupSubscription() {
+    if (subscription != null) {
+      if (!subscription.isUnsubscribed()) subscription.unsubscribe();
+      subscription = null;
     }
+  }
 
-    /** Unsubscribe all. Need to call this to clean up rx resources. */
-    private void cleanupSubscription() {
-        if (subscription != null) {
-            if (!subscription.isUnsubscribed()) subscription.unsubscribe();
-            subscription = null;
-        }
+  @Override
+  public void onMapReady(GoogleMap gMap) {
+    this.googleMap = gMap;
+  }
+
+  /**
+   * Update the marker on the map with the new coordinate from the stream.
+   */
+  private void updateMap(Coordinate coordinate) {
+    LatLng latLng = new LatLng(coordinate.lat(), coordinate.lon());
+
+    if (deviceMarker == null) {
+      MarkerOptions deviceMarkerOptions = new MarkerOptions()
+          .position(latLng)
+          .title(device.name());
+
+      deviceMarker = googleMap.addMarker(deviceMarkerOptions);
+    } else {
+      animateMarker(deviceMarker, latLng);
     }
+    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
+  }
 
-    @Override
-    public void onMapReady(GoogleMap gMap) {
-        this.googleMap = gMap;
+  public void animateMarker(Marker marker, LatLng finalPosition) {
+    TypeEvaluator<LatLng> typeEvaluator = new TypeEvaluator<LatLng>() {
+      @Override
+      public LatLng evaluate(float fraction, LatLng startValue, LatLng endValue) {
+        return interpolate(fraction, startValue, endValue);
+      }
+    };
+    Property<Marker, LatLng> property = Property.of(Marker.class, LatLng.class, "position");
+    ObjectAnimator animator = ObjectAnimator.ofObject(marker, property, typeEvaluator, finalPosition);
+    animator.setDuration(3500);
+    animator.start();
+  }
+
+  public LatLng interpolate(float fraction, LatLng a, LatLng b) {
+    double lat = (b.latitude - a.latitude) * fraction + a.latitude;
+    double lngDelta = b.longitude - a.longitude;
+
+    // Take the shortest path across the 180th meridian.
+    if (Math.abs(lngDelta) > 180) {
+      lngDelta -= Math.signum(lngDelta) * 360;
     }
-
-    /** Update the marker on the map with the new coordinate from the stream. */
-    private void updateMap(Coordinate coordinate){
-        LatLng latLng = new LatLng(coordinate.lat(), coordinate.lon());
-
-        if(deviceMarker == null){
-            MarkerOptions deviceMarkerOptions = new MarkerOptions()
-                    .position(latLng)
-                    .title(device.name());
-
-            deviceMarker = googleMap.addMarker(deviceMarkerOptions);
-        }else{
-            animateMarker(deviceMarker, latLng);
-        }
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
-    }
-
-    public void animateMarker(Marker marker, LatLng finalPosition) {
-        TypeEvaluator<LatLng> typeEvaluator = new TypeEvaluator<LatLng>() {
-            @Override
-            public LatLng evaluate(float fraction, LatLng startValue, LatLng endValue) {
-                return interpolate(fraction, startValue, endValue);
-            }
-        };
-        Property<Marker, LatLng> property = Property.of(Marker.class, LatLng.class, "position");
-        ObjectAnimator animator = ObjectAnimator.ofObject(marker, property, typeEvaluator, finalPosition);
-        animator.setDuration(3500);
-        animator.start();
-    }
-
-    public LatLng interpolate(float fraction, LatLng a, LatLng b) {
-        double lat = (b.latitude - a.latitude) * fraction + a.latitude;
-        double lngDelta = b.longitude - a.longitude;
-
-        // Take the shortest path across the 180th meridian.
-        if (Math.abs(lngDelta) > 180) {
-            lngDelta -= Math.signum(lngDelta) * 360;
-        }
-        double lng = lngDelta * fraction + a.longitude;
-        return new LatLng(lat, lng);
-    }
+    double lng = lngDelta * fraction + a.longitude;
+    return new LatLng(lat, lng);
+  }
 }
